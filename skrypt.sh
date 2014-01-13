7af6771e622d016f5aa8614cc74132292d82adf7
panUFO <kliwinski93@gmail.com>
2014-01-13 13:32:35 +0100

    Create skrypt.sh

---
+++ skrypt.sh
@@ -0,0 +1,27 @@
+SKRYPT
+```sh
+!/usr/bin/perl -w
+system "clear";
+print "PROGRAM GENERUJĄCY HASŁA \n";
+print "Podaj długość hasła ";
+$dlugosc = <STDIN>;
+if ($dlugosc lt "1")
+{ print "NIE WPROWADZONO ZADNEJ DLUGOSCI, KONIEC PROGRAMU\n";
+exit}
+
+print "ILE HASEŁ WYGENEROWAĆ? ";
+$powt =<STDIN>;
+if ($powt lt "1")
+{ print "BRAK ILOŚCI, KONIEC PROGRAMU\n";
+exit}
+print "NO TO GENERUJEMY! <3\n";
+$ile = $powt;
+for (1 ..$powt)
+{
+{ @lines = `pwgen -s -y $dlugosc`;print "hasło:     ";}
+foreach (@lines) #-y, (znaki specjalne) -s (bezpieczne, trudne do zapamietania)
+
+{print;}
+}
+
+```
